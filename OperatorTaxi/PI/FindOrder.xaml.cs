5ba0b16822ca411be432b2f3bd78050c7cfb9db1
Vladyslav Palvyk <ibrauningi15@gmail.com>
2019-12-13 01:02:54 +0200

    Add documentations and final edit project.(And correct my last error)

--- OperatorTaxi/PI/FindOrder.xaml.cs
+++ OperatorTaxi/PI/FindOrder.xaml.cs
@@ -1,66 +1,84 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Input;
+using System.Windows.Controls;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Data;
 
 namespace PI
 {
     /// <summary>
-    /// Клас MainWindow, створений для взаємодії з MainWindow.xaml.
+    /// Клас FindOrder, створений для взаємодії з FindOrder.xaml
     /// </summary>
-
-    public partial class MainWindow : Window
+    public partial class FindOrder : Page
     {
-        public MainWindow()
+        public FindOrder()
         {
             InitializeComponent();
         }
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        public FindOrder(string Login)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            InitializeComponent();
+            this.Login = Login;
+        }
+        public string Login { get; set; }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            CountAdult.SelectedIndex = 0;
+            CountChild.SelectedIndex = 0;
+            CountInfant.SelectedIndex = 0;
+            DepartTaxi.Items.Clear();
+            ArrivalTaxi.Items.Clear();
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DataBaseOperatorTaxi;Integrated Security=True";
+            try
             {
-                DragMove();
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    con.Open();
+                    cmd.Connection = con;
+                    cmd.CommandText = "SELECT DISTINCT City FROM AddOrder Order by City";
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        DepartTaxi.Items.Add(dr["City"]);
+                        ArrivalTaxi.Items.Add(dr["City"]);
+                    }
+                    dr.Close();
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
             }
         }
+
         /// <summary>
-        /// Login_Click-функція,визначає параметри входу для простого користувача та адміністратора.
+        /// SearchTaxi_Click витягує шукані поїздки з БД.
         /// </summary>
-        /// <param name="sender">Об'єкти</param>
+        /// <param name="sender">Об'єкт</param>
         /// <param name="e">Маршрутизована подія</param>
-        private void Login_Click(object sender, RoutedEventArgs e)
+
+        private void SearchTaxi_Click(object sender, RoutedEventArgs e)
         {
-            if (Login1.Text == "admin" && Password.Password == "1111")
-            {
-                Admin adminWindow = new Admin();
-                this.Visibility = Visibility.Hidden;
-                adminWindow.Show();
-            }
-            else
+            if (DepartTaxi.Text != "" && ArrivalTaxi.Text != "" && DatePicker.Text != "")
             {
                 try
                 {
                     string connectionString = ConfigurationManager.ConnectionStrings["MainConnection"].ConnectionString;
-                    string query = $"SELECT Count(*) from Customer where Login = '{Login1.Text.ToString()}' AND Password = '{Password.Password}'";
-
-
+                    string query = $"SELECT Id,DepartCity,ArriveCity,convert(varchar(10),DepartDate,104) as 'DepartDate'," +
+                        $"convert(varchar(10),ArriveDate,104) as 'ArrivalDate',CAST(DepartTime AS CHAR(5)) as 'DepartTime',CAST(ArriveTime AS CHAR(5)) as 'ArriveTime',TAXIXID as 'TaxiID',DriverName as 'Phone number' FROM InfoOrder " +
+                        $"where DepartCity = '{DepartTaxi.Text}' AND ArriveCity = '{ArrivalTaxi.Text}' and convert(varchar(10),DepartDate,104) = '{Convert.ToDateTime(DatePicker.Text).ToString("dd.MM.yyyy")}'";
                     using (SqlConnection connection = new SqlConnection(connectionString))
                     {
                         connection.Open();
-                        SqlCommand command = new SqlCommand(query, connection);
-                        object count = command.ExecuteScalar();
-                        if ((int)count == 1)
-                        {
-                            Menu menuWindow = new Menu(Login1.Text);
-                            this.Visibility = Visibility.Hidden;
-                            menuWindow.Show();
-                        }
-                        else
-                        {
-                            MessageBox.Show("Wrong login or password");
-                        }
-                        connection.Close();
+                        SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                        DataSet ds = new DataSet();
+                        adapter.Fill(ds);
+                        dataGrid.ItemsSource = ds.Tables[0].DefaultView;
                     }
                 }
                 catch (Exception ee)
@@ -68,22 +86,12 @@ namespace PI
                     MessageBox.Show(ee.Message);
                 }
             }
-        }
-        /// <summary>
-        /// Registration-Click функція, яка викликає вікно реєстрації Registration.
-        /// </summary>
-        /// <param name="sender">Об'єкт</param>
-        /// <param name="e">Маршрутизована подія</param>
-        private void Registation_Click(object sender, RoutedEventArgs e)
-        {
-            Registration registrationWindow = new Registration();
-            this.Visibility = Visibility.Hidden;
-            registrationWindow.Show();
+            else
+            {
+                MessageBox.Show("Заповніть всі поля");
+            }
         }
 
-        private void ExitButton_Click_1(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
+
     }
 }
